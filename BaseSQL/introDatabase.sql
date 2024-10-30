075e948ba8c9206b392d9cd3aa861f4c9a48f3b2
kgbfirehawk <belinak@oregonstate.edu>
2024-10-30 17:55:00 -0500

    Added foregin key checks disable/enable

--- BaseSQL/introDatabase.sql
+++ BaseSQL/introDatabase.sql
@@ -1,3 +1,6 @@
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
 CREATE OR REPLACE TABLE Libraries (
     libraryID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     libraryName VARCHAR(50) NOT NULL,
@@ -12,13 +15,13 @@ CREATE OR REPLACE TABLE Books (
     bookAuthor VARCHAR(50) NOT NULL,
     bookGenre VARCHAR(50) NOT NULL,
     Libraries_libraryID INT NOT NULL,
-    FOREIGN KEY (Libraries_libraryID) REFERENCES Libraries(libraryID)
+    CONSTRAINT fk_books_library FOREIGN KEY (Libraries_libraryID) REFERENCES Libraries(libraryID) ON DELETE CASCADE
 );
 
 CREATE OR REPLACE TABLE Borrowers (
     userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     userName VARCHAR(50) NOT NULL,
-    userAddress VARCHAR(145) NOT NULL,
+    userAddress VARCHAR(50) NOT NULL,
     userPhone INT NOT NULL
 );
 
@@ -28,26 +31,30 @@ CREATE OR REPLACE TABLE Staff (
     staffTitle VARCHAR(50) NOT NULL,
     staffExtension INT,
     Libraries_libraryID INT NOT NULL,
-    FOREIGN KEY (Libraries_libraryID) REFERENCES Libraries(libraryID)
+    CONSTRAINT fk_staff_library FOREIGN KEY (Libraries_libraryID) REFERENCES Libraries(libraryID) ON DELETE CASCADE
 );
 
 CREATE OR REPLACE TABLE Checkouts (
+    checkoutID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     dueDate DATE NOT NULL,
     isReturn TINYINT,
     Libraries_libraryID INT NOT NULL,
     Books_bookID INT NOT NULL,
     Borrowers_userID INT NOT NULL,
     Staff_staffID INT NOT NULL,
-    FOREIGN KEY (Libraries_libraryID) REFERENCES Libraries(libraryID),
-    FOREIGN KEY (Books_bookID) REFERENCES Books(bookID),
-    FOREIGN KEY (Borrowers_userID) REFERENCES Borrowers(userID),
-    FOREIGN KEY (Staff_staffID) REFERENCES Staff(staffID)
+    CONSTRAINT fk_checkouts_library FOREIGN KEY (Libraries_libraryID) REFERENCES Libraries(libraryID) ON DELETE CASCADE,
+    CONSTRAINT fk_checkouts_book FOREIGN KEY (Books_bookID) REFERENCES Books(bookID) ON DELETE CASCADE,
+    CONSTRAINT fk_checkouts_borrower FOREIGN KEY (Borrowers_userID) REFERENCES Borrowers(userID) ON DELETE CASCADE,
+    CONSTRAINT fk_checkouts_staff FOREIGN KEY (Staff_staffID) REFERENCES Staff(staffID) ON DELETE CASCADE
 );
 
 CREATE OR REPLACE TABLE Books_has_Borrowers (
     Books_bookID INT NOT NULL,
     Borrowers_userID INT NOT NULL,
     PRIMARY KEY (Books_bookID, Borrowers_userID),
-    FOREIGN KEY (Books_bookID) REFERENCES Books(bookID),
-    FOREIGN KEY (Borrowers_userID) REFERENCES Borrowers(userID)
+    CONSTRAINT fk_books_has_borrowers_book FOREIGN KEY (Books_bookID) REFERENCES Books(bookID) ON DELETE CASCADE,
+    CONSTRAINT fk_books_has_borrowers_borrower FOREIGN KEY (Borrowers_userID) REFERENCES Borrowers(userID) ON DELETE CASCADE
 );
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
