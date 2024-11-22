74f2d2e7ccf2cbfaa8bdc60af94d9c0848032ceb
DreamShardz <78979840+DreamShardz@users.noreply.github.com>
2024-11-21 20:51:09 -0800

    Update db-connector.js
    updated code citations

--- librarydb/database/db-connector.js
+++ librarydb/database/db-connector.js
@@ -1,3 +1,11 @@
+ /* Citation for the following function:
+Date: 11/21/2024
+Adapted from CS 340: nodejs-starter-app (db-connector.js)
+Majority of the code was copied from the sample code given, and modified based on individual project tables. 
+Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%200%20-%20Setting%20Up%20Node.js
+*/
+
+
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
