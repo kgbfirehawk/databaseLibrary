e862495cff516e81ebf1b229dad6526cad2e22c0
kgbfirehawk <belinak@oregonstate.edu>
2024-11-21 19:33:56 -0600

    Added Staff CRUD operations. Added files staff.hbs, staffRoutes.js and changes to app.jslibrarydb/database/db-connector.js

--- librarydb/app.js
+++ librarydb/app.js
@@ -7,7 +7,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 8728;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 8727;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
 var db = require('./database/db-connector')
@@ -17,11 +17,20 @@ const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+const hbs = require('handlebars');
+hbs.registerHelper('eq', function(a, b) {
+  return a === b;
+});
 
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
 
 /*
     ROUTES
 */
+const staffRoutes = require('./routes/staffRoutes');  // Import staff routes
+app.use('/', staffRoutes);  // Use staff routes
+
 app.get('/', function(req, res)
     {  
         let query1 = "SELECT * FROM Staff;";               // Define our query
---
+++ librarydb/routes/staffRoutes.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../database/db-connector');  
+
+// GET route for the main /staff page
+router.get('/staff', function(req, res) {
+    let queryStaff = "SELECT * FROM Staff;";  // SQL query to get all staff members
+    let queryLibraries = "SELECT * FROM Libraries;";  // SQL query to get all libraries
+
+    db.pool.query(queryStaff, function(error, staffRows, fields) {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500); 
+        } else {
+            db.pool.query(queryLibraries, function(libError, libraryRows, fields) {
+                if (libError) {
+                    console.error(libError);
+                    res.sendStatus(500);  
+                } else {
+                    res.render('staff', {
+                        data: staffRows,
+                        libraries: libraryRows
+                    });
+                }
+            });
+        }
+    });
+});
+
+// GET route for editing a staff member
+router.get('/staff/update/:staffID', function(req, res) {
+    let staffID = req.params.staffID;
+
+    let queryStaff = `SELECT * FROM Staff WHERE staffID = ?;`;
+    let queryLibraries = "SELECT * FROM Libraries;";
+
+    db.pool.query(queryStaff, [staffID], function(error, staffRows, fields) {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500);
+        } else {
+            db.pool.query(queryLibraries, function(libError, libraryRows, fields) {
+                if (libError) {
+                    console.error(libError);
+                    res.sendStatus(500);
+                } else {
+                    res.render('staff', {
+                        data: staffRows,
+                        libraries: libraryRows,
+                        editData: staffRows[0]  // Send the selected staff data for editing
+                    });
+                }
+            });
+        }
+    });
+});
+
+// POST route for adding a new staff member
+router.post('/staff/add', function(req, res) {
+    let data = req.body;
+
+    let query = `INSERT INTO Staff (staffName, staffTitle, staffExtension, librariesLibraryID) VALUES (?, ?, ?, ?)`;
+    let inserts = [data.staffName, data.staffTitle, data.staffExtension, data.librariesLibraryID];
+
+    db.pool.query(query, inserts, function(error, rows, fields) {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500); 
+        } else {
+            res.redirect('/staff');  // Redirect back to /staff after adding a staff member
+        }
+    });
+});
+
+// POST route for deleting a staff member
+router.post('/staff/delete/:staffID', function(req, res) {
+    let staffID = req.params.staffID;
+
+    let query = `DELETE FROM Staff WHERE staffID = ?`;
+    let inserts = [staffID];
+
+    db.pool.query(query, inserts, function(error, rows, fields) {
+        if (error) {
+            console.error(error);
+            if (error.errno === 1451) {
+                res.send("Cannot delete staff member. This staff member is referenced in other records."); // Handle FK constraint error
+            } else {
+                res.sendStatus(500);  
+            }
+        } else {
+            res.redirect('/staff');  // Redirect back to /staff after deleting
+        }
+    });
+});
+
+// POST route for updating a staff member
+router.post('/staff/update/:staffID', function(req, res) {
+    let staffID = req.params.staffID;
+    let data = req.body;
+
+    let query = `UPDATE Staff SET staffName = ?, staffTitle = ?, staffExtension = ?, librariesLibraryID = ? WHERE staffID = ?`;
+    let inserts = [data.staffName, data.staffTitle, data.staffExtension, data.librariesLibraryID, staffID];
+
+    db.pool.query(query, inserts, function(error, rows, fields) {
+        if (error) {
+            console.error(error);
+            res.sendStatus(500);  
+        } else {
+            res.redirect('/staff');  // Redirect back to /staff after updating
+        }
+    });
+});
+
+module.exports = router;
