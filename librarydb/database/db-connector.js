129b5cddd44b6515927f1cd65ceced64e851947a
kgbfirehawk <belinak@oregonstate.edu>
2024-12-18 04:59:57 -0600

    Changes to make it safe for public

--- librarydb/database/db-connector.js
+++ librarydb/database/db-connector.js
@@ -1,21 +1,22 @@
- /* Citation for the following function:
+/* Citation for the following function:
 Date: 11/21/2024
 Adapted from CS 340: nodejs-starter-app (db-connector.js)
 Majority of the code was copied from the sample code given, and modified based on individual project tables. 
 Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%200%20-%20Setting%20Up%20Node.js
 */
 
+require('dotenv').config({ path: '../.env' }); // Load environment variables from .env file
 
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
-    connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_jackmack',
-    password        : '3577',
-    database        : 'cs340_jackmack'
+    connectionLimit : process.env.DB_CONNECTION_LIMIT,
+    host            : process.env.DB_HOST,
+    user            : process.env.DB_USER,
+    password        : process.env.DB_PASSWORD,
+    database        : process.env.DB_DATABASE
 })
 
 // Export it for use in our application
